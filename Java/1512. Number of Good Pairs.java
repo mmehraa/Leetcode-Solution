d2303166a6dc81afa5510e84e15c24a6a458bfd2
Mayank Mehra <139584956+mmehraa@users.noreply.github.com>
2023-10-23 18:07:19 +0530

    Create 1512. Number of Good Pairs.java

---
+++ Java/1512. Number of Good Pairs.java
@@ -0,0 +1,24 @@
+/*
+    1512. Number of Good Pairs ( https://leetcode.com/problems/number-of-good-pairs )
+        Given an array of integers nums, return the number of good pairs. A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+        Example 1:
+              Input: nums = [1,2,3,1,1,3]
+              Output: 4
+              Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+*/
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if ( i < j && nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
