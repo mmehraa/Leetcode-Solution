e2390d237236859f82fed79957a7858a1a7aa6a0
Mayank Mehra <139584956+mmehraa@users.noreply.github.com>
2023-10-23 17:56:04 +0530

    0009. Palindrome Number

---
+++ Java/0009 - Palindrome - Number.java
@@ -0,0 +1,28 @@
+/*
+    9. Palindrome Number
+        https://leetcode.com/problems/palindrome-number
+        Given an integer x, return true if x is a palindrome, and false otherwise.
+        Example 1:
+            Input: x = 121
+            Output: true
+            Explanation: 121 reads as 121 from left to right and from right to left.
+*/
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        int original, remainder, reversed = 0;
+        original = x;
+
+        while (x > 0){
+            remainder = x % 10;
+            reversed = reversed*10 + remainder;
+            x /= 10;
+        }
+
+        if (reversed == original){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
